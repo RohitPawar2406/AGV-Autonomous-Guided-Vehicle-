7aaa52690bdd036d9094375baecb93b87ee18d3b
Rohit Pawar <59295507+RohitPawar2406@users.noreply.github.com>
2021-01-15 20:20:38 +0530

    f

--- App Development/main.js
+++ App Development/main.js
@@ -1,19 +1,23 @@
 const express = require('express')
+const path = require('path')
+
 //const date = require('date-and-time');
 const app = express()
-
+const server=require('http').Server(app)
+const io= require('socket.io')(server)
 const now = new Date();
-const port =3001        // Port Number of Nodejs
+const port =5005       // Port Number of Nodejs
 
 app.use(express.json({limit:'500tb'}))
-
 
 app.post('/',async(req,res)=>{
     live_stream_images = req.body.Image
     console.log(typeof(req.body.Image))  
+    io.emit('image',live_stream_images)
     console.log("================================================================")
     console.log("================================================================")
-    res.send(req.body.Image)
+    //res.send(req.body.Image)
+    res.sendFile(path.join(__dirname,'index1.html'))
 })
 
 
@@ -24,7 +28,15 @@ app.get('/sending',async(req,res)=>{
   console.log(a.Forward,typeof(a.Forward))
   res.send(b)
 })
+/*app.get('/liveonHTML',(req,res)=>{
+  res.sendFile(path.join(__dirname,'index1.html'))
+})*/
 
-app.listen(port,()=>{
+/*setInterval(()=>{
+  image = 
+  io.emit('image','some data')
+},1000)*/
+
+server.listen(port,()=>{
     console.log("Starting on port: "+port)
 })
